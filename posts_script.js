8bfffae10eb736d49a6f730ce025de7f030eb282
Johnny <jhume001@gmail.com>
2024-10-23 20:15:46 -0400

    Add script to get posts

---
+++ posts_script.js
@@ -0,0 +1,45 @@
+function getPosts(){
+    fetch("http://localhost:5000/posts")
+      .then(response => {
+	if (!response.ok) {
+	  throw new Error('Network response was not ok');
+	}
+	return response.json();
+      })
+      .then(data => {
+	data.forEach((post) => createPostCard(post));
+      })
+      .catch(error => {
+	console.error('Error:', error);
+      });
+}
+
+function createPostCard(post){
+
+    const div = document.createElement("div");
+
+    const title = document.createElement("H1");
+    title.textContent = "Title: " + post["title"]
+
+    const pickup = document.createElement("p");
+    pickup.textContent = "pickup: " + post["pickup"]
+
+    const dropoff = document.createElement("p");
+    dropoff.textContent = "dropoff: " + post["dropoff"]
+
+    const date = document.createElement("p");
+    date.textContent = "date: " + post["date"]
+
+    const num_passengers = document.createElement("p");
+    num_passengers.textContent = "num_passengers: " + post["num_passengers"]
+
+    div.appendChild(title)
+    div.appendChild(pickup)
+    div.appendChild(dropoff)
+    div.appendChild(date)
+    div.appendChild(num_passengers)
+
+    document.getElementById("postsDisplay").appendChild(div)
+}
+
+getPosts()
