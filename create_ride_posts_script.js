c8a4367628cab4a5eac3fdd6b2932428a8ac390f
Johnny <jhume001@gmail.com>
2024-11-26 14:02:27 +0000

    Add create general post form

--- create_ride_posts_script.js
+++ create_ride_posts_script.js
@@ -1,7 +1,7 @@
-function sendCreatePostRequest(){
+function sendCreateRidePostRequest(){
     fetch("http://localhost:5000/ride_post", {
 	method: "POST",
-	body: JSON.stringify(createPostObj()),
+	body: JSON.stringify(createRidePostObj()),
 	headers: { "content-type": "application/json" }
     })
       .then(response => {
@@ -19,7 +19,28 @@ function sendCreatePostRequest(){
       });
 }
 
-function populateWithRidePostFields(){
+function sendCreateGeneralPostRequest(){
+    fetch("http://localhost:5000/general_post", {
+	method: "POST",
+	body: JSON.stringify(createGeneralPostObj()),
+	headers: { "content-type": "application/json" }
+    })
+      .then(response => {
+	if (!response.ok) {
+	  throw new Error('Network response was not ok');
+	}
+	return response.json();
+      })
+      .then(data => {
+	document.getElementById("statusOutput").innerText = "Post Created!"
+	console.log(data)
+      })
+      .catch(error => {
+	console.error('Error:', error);
+      });
+}
+
+function buildGeneralPostForm(){
 
     userTypes = ["hiker", "trail_angel"]
 
@@ -32,7 +53,80 @@ function populateWithRidePostFields(){
     userIdInput.id = "userId";
 
     const divUserId = document.createElement("div")
+    divUserId.appendChild(userIdLabel)
     divUserId.appendChild(userIdInput)
+
+    const userTypeLabel = document.createElement("label")
+    userTypeLabel.setAttribute("for", "userType")
+    userTypeLabel.textContent = "User Type:"
+
+    const userTypeInput = document.createElement("select");
+    userTypeInput.id = "userType";
+
+    for (var i = 0; i < userTypes.length; i++) {
+	var option = document.createElement("option");
+	option.value = userTypes[i];
+	option.text = userTypes[i];
+	userTypeInput.appendChild(option);
+    }
+
+    const divUserType = document.createElement("div")
+    divUserType.appendChild(userTypeLabel)
+    divUserType.appendChild(userTypeInput)
+
+    const titleLabel = document.createElement("label")
+    titleLabel.setAttribute("for", "title")
+    titleLabel.textContent = "Title:"
+
+    const titleInput = document.createElement("input");
+    titleInput.type = "text";
+    titleInput.id = "title";
+
+    const divTitle = document.createElement("div")
+    divTitle.appendChild(titleLabel)
+    divTitle.appendChild(titleInput)
+
+    const textLabel = document.createElement("label")
+    textLabel.setAttribute("for", "text")
+    textLabel.textContent = "Text:"
+
+    const textInput = document.createElement("textarea");
+    textInput.cols = "50";
+    textInput.rows = "10";
+    textInput.id = "text";
+
+    const divText = document.createElement("div")
+    divText.appendChild(textLabel)
+    divText.appendChild(textInput)
+
+    const submitButton = document.createElement("button")
+    submitButton.textContent = "Submit"
+    submitButton.onclick = () => {sendCreateGeneralPostRequest()}
+
+    document.getElementById("createPostForm").innerHTML = ""
+
+    document.getElementById("createPostForm").appendChild(divUserId)
+    document.getElementById("createPostForm").appendChild(divUserType)
+    document.getElementById("createPostForm").appendChild(divTitle)
+    document.getElementById("createPostForm").appendChild(divText)
+    document.getElementById("createPostForm").appendChild(submitButton)
+    
+}
+
+function buildRidePostForm(){
+
+    userTypes = ["hiker", "trail_angel"]
+
+    const userIdLabel = document.createElement("label")
+    userIdLabel.setAttribute("for", "userId")
+    userIdLabel.textContent = "User Id:"
+
+    const userIdInput = document.createElement("input");
+    userIdInput.type = "text";
+    userIdInput.id = "userId";
+
+    const divUserId = document.createElement("div")
+    divUserId.appendChild(userIdLabel)
     divUserId.appendChild(userIdInput)
 
     const userTypeLabel = document.createElement("label")
@@ -113,6 +207,11 @@ function populateWithRidePostFields(){
     divNumPassengers.appendChild(numPassengersLabel)
     divNumPassengers.appendChild(numPassengersInput)
 
+    const submitButton = document.createElement("button")
+    submitButton.textContent = "Submit"
+    submitButton.onclick = () => {sendCreateRidePostRequest()}
+
+    document.getElementById("createPostForm").innerHTML = ""
 
     document.getElementById("createPostForm").appendChild(divUserId)
     document.getElementById("createPostForm").appendChild(divUserType)
@@ -121,9 +220,10 @@ function populateWithRidePostFields(){
     document.getElementById("createPostForm").appendChild(divDropoff)
     document.getElementById("createPostForm").appendChild(divDate)
     document.getElementById("createPostForm").appendChild(divNumPassengers)
+    document.getElementById("createPostForm").appendChild(submitButton)
 }
 
-function createPostObj(){
+function createRidePostObj(){
 
     userId = document.getElementById("userId").value;
     userType = document.getElementById("userType").value;
@@ -143,4 +243,32 @@ function createPostObj(){
 	num_passengers: num_passengers
     }
 }
-populateWithRidePostFields()
+
+function createGeneralPostObj(){
+
+    userId = document.getElementById("userId").value;
+    userType = document.getElementById("userType").value;
+    title = document.getElementById("title").value;
+    text = document.getElementById("text").value;
+
+    return {
+	user_id: userId,
+	user_type: userType,
+	title: title,
+	text: text
+    }
+}
+
+document.getElementById("postType").selectedIndex = 0
+document.getElementById("postType").addEventListener("change", function() {
+    value = document.getElementById("postType").value
+    if (value == "ride"){
+	buildRidePostForm();
+    }
+    else if (value == "general"){
+	buildGeneralPostForm();
+    }
+    else{
+	throw new Error("Unhandled Post Type Selected")
+    }
+});
